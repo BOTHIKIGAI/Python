ebe8b7dd4d324bda50a9e8b1f9e8504345d96e19
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2022-12-28 17:00:46 -0500

    Ruleta Rusa V 0.5
    para aprender a usar los ciclos y los if empecé la creación de una ruleta rusa

---
+++ 1. Ruleta_Rusa.py
@@ -0,0 +1,69 @@
+"""
+
+28 / 12 / 2022
+
+
+/////////     /       /   /           /////////   /////////   /////////      /////////     /       /   /////////   /////////
+/       /     /       /   /           /               /       /       /      /       /     /       /   /           /       /
+/       /     /       /   /           /               /       /       /      /       /     /       /   /           /       /
+/////////     /       /   /           /////////       /       /////////      /////////     /       /   /////////   /////////
+/       /     /       /   /           /               /       /       /      /       /     /       /           /   /       /
+/        /    /       /   /           /               /       /       /      /        /    /       /           /   /       /
+/         /   /////////   /////////   /////////       /       /       /      /         /   /////////   /////////   /       /
+
+La ruleta rusa es un juego de azar potencialmente mortal, que consiste en que un jugador coloque una o más balas dentro de un tambor de revólver, gire el cilindro (sin ver en dónde quedó el proyectil), coloque el 
+cañón en su sien y presione el gatillo. Se juega generalmente entre dos o más personas. El objetivo es sobrevivir y quedarse con el dinero o la especie de valor a jugar.
+
+Generalmente se inicia en una mesa. Uno de los jugadores toma un revólver (puede ser de 5 o 6 cartuchos) y abre el tambor. En él pone uno o más proyectiles, dependiendo de la cantidad de dinero apostado. Luego gira 
+el tambor al azar, cerrándolo rápidamente de modo que ninguno de los jugadores pueda ver en qué recámara se encuentran los cartuchos. Por turnos los jugadores colocan la boca del cañón sobre su sien y aprietan el 
+gatillo sin mover el arma. Si ningún cartucho es disparado, el jugador continúa en el juego y el revólver pasa a su compañero. Si el jugador se salva, el revólver es pasado al siguiente jugador hasta que a uno de 
+ellos le toque el cartucho y reciba el disparo.
+
+
+"""
+
+# Pregunta 1 - Iniciamos el Juego
+
+pregunta1 = str(input("Iniciamos el juego? (Si o No): "))
+
+while pregunta1 != "Si" and pregunta1 != "No":
+    print("")
+    print ("Disculpe, la respuesta dada no es correcta, recuerde que es Si o No")
+    pregunta1 = str(input("Iniciamos el juego?: "))
+
+if pregunta1 == "No":
+    print("")
+    print("Esta bien, nos veremos en otro momento")
+else:
+    print("")
+    print("Perfecto, iniciemos...")
+
+    # Pregunta 2 - Cantidad de Participanes
+
+    print("")
+    pregunta2 = int(input("Ingrese la cantidad de participantes, recuerda que debe ser un numero (max. 5): "))
+    while pregunta2 <= 0 or pregunta2 >= 6:
+        if pregunta2 <= 0:
+            print ("Cero participantes?")
+            pregunta2 = int(input("Ingrese la cantidad de participantes, recuerda que debe ser un numero (max. 5): "))
+        else:
+            print ("El numero de participantes maximo es de 5")
+            pregunta2 = int(input("Ingrese la cantidad de participantes, recuerda que debe ser un numero (max. 5): "))
+    print("")
+    
+    # Pregunta 3 - Pregunta de Curiosdad - Porque el maximo es de 5 participantes
+            
+    pregunta3 = str(input("Deseas saber por que el maximo de participantes es de 5? (Si o No): "))
+
+    while pregunta3 != "Si" and pregunta3 != "No":
+        print ("Disculpe, la respuesta dada no es correcta, recuerde que es Si o No")
+        pregunta3 = str(input("Deseas saber por que el maximo de participantes es de 5? (Si o No): "))
+    
+    if pregunta3 == "Si":
+        print ("La suerte termina en donde empieza...")
+    else:
+        print ("A veces la ignorancia es mucho mas satisfactoria...")
+
+    
+
+    
