63f78ffb569885829415d18e33fea6d9444d8a70
Juan Esteban Cajiao Madero <estebax89@gmail.com>
2023-02-27 22:52:09 -0500

    Update 12. Juanes_0.1%.py

--- 0. Mis propios desafios/1. Juanes_Talk/1. Juanes_talk   0.1 %/12. Juanes_0.1%.py
+++ 0. Mis propios desafios/1. Juanes_Talk/1. Juanes_talk   0.1 %/12. Juanes_0.1%.py
@@ -12,7 +12,8 @@ import speech_recognition
     # speech_recognition libreria de reconocimiento de voz
 import pywhatkit
     # pywhatkit libreria para la reproduccion de video por medio de youtube y enviar mensajes a youtube
-
+import wikipedia
+    # wikipedia libreria para realizar busquedas en la pagina de esta misma 
 import datetime
     # datetime proporciona clases para manipular fechas y horas
 import calendar
@@ -48,8 +49,8 @@ Las preguntas que puedes hacerle a Juanes_talk son las siguientes
                                                                   
                                                                                         (musica para estudiar)
                                                                                   
-5. Mensajes Whatsapp                                                     
-    - comando de voz = saludo mama                                        
+5. Mensajes Whatsapp                                                     6. Busqueadas                                
+    - comando de voz = saludo mama                                         - comando de voz = buscar
     - comando de voz = saludo Papa                                           
     - comando de voz = saludo Grupo                                        
 
@@ -143,6 +144,27 @@ while continuar == 1:
         pywhatkit.sendwhatmsg("+57", "Hola papa, como estas ?", hora, min)
         engine = pyttsx3.init()
         engine.say("Enviando mensaje papa")
+        engine.runAndWait()
+    
+    elif x == "buscar":
+        r = speech_recognition.Recognizer()
+        
+        with speech_recognition.Microphone() as source:
+            print("Di algo: ")
+            audio = r.listen(source)
+        
+        try:
+            texto = r.recognize_google(audio)
+            print ("Tu dijiste {}", format(texto)) 
+            x = (format(texto)) 
+
+        except:
+            print("Perdon no te puedo escuchar")
+        
+        print wikipedia.summary(format(texto))
+
+        engine = pyttsx3.init()
+        engine.say("Realizando busqueda ")
         engine.runAndWait()
 
     else:
