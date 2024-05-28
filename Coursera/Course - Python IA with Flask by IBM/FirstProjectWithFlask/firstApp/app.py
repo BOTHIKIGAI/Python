aad2b740ed3ebfad995e7e948db9da0dad964e57
Juan Esteban Cajiao Madero <96344956+BOTHIKIGAI@users.noreply.github.com>
2024-05-28 07:41:18 -0500

    Make a response with response from flask import make_response

--- Coursera/Course - Python IA with Flask by IBM/FirstProjectWithFlask/firstApp/app.py
+++ Coursera/Course - Python IA with Flask by IBM/FirstProjectWithFlask/firstApp/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
@@ -6,6 +6,34 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return "<h1>Hello world</h1>"
+
+
+@app.route('/hello', methods=['GET', 'POST'])
+def hello():
+    response = make_response()
+    response.status_code = 202
+    response.headers['content-type'] = 'text/plain'
+    return response
+
+
+@app.route('/greet/<name>')
+def greet(name):
+    return f"Hello {name}"
+
+
+@app.route('/add/<int:number1>/<int:number2>')
+def add(number1, number2):
+    return f'{number1} + {number2} = {number1 + number2}'
+
+
+@app.route('/handle_url_params')
+def handle_params():
+    if 'greeting' in request.args.keys() and 'name' in request.args.keys():
+        greeting = request.args.get('greeting')
+        name = request.args.get('name')
+        return f'{greeting}, {name}'
+    else:
+        return 'Some parameters are missing'
 
 
 if __name__ == '__main__':
